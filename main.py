85015e66c6087a839ba20583fdf0e309a6885ef8
ianadivane2006@gmail.com <ianadivane2006@gmail.com>
2025-07-11 18:54:26 +0300

    changed logick for collect_data (to towards multiprocessing)

--- main.py
+++ main.py
@@ -1,54 +1,15 @@
-import sqlite3
-import os
+from multiprocessing import Process, Queue
 from datetime import datetime
 from pathlib import Path
+import queue
+import sqlite3
+import os
 import ctypes
-from multiprocessing import Process
 # import sys
 # from tqdm import tqdm
 
 SLASH = r"\\"[0]
 LOCAL_C = r'C:' + SLASH
-
-
-def convert_to_short_path(path: str) -> str:
-    buf = ctypes.create_unicode_buffer(260)
-    ctypes.windll.kernel32.GetShortPathNameW(r'\\?' + SLASH + path, buf, 260)
-    return buf.value
-
-
-def scan_directory(cursor, path):
-    short_path = convert_to_short_path(path)
-    try:
-        files = os.listdir(short_path)
-    except PermissionError:
-        print('Access denied[+] ', path, short_path)
-        return
-    except Exception as e:
-        print("unexpected error - ", e)
-        print(path, short_path)
-        exit()
-
-
-    for file in files:
-        file_in_question = short_path + SLASH + file
-
-        if os.path.isdir(file_in_question):
-            try:
-                add_values(cursor, "DataBase", (path + SLASH + file, path, short_path, file,
-                                                "directory", put_time_creation_to_table(file_in_question), os.path.getmtime(file_in_question)))
-                scan_directory(cursor, path + SLASH + file)
-            except Exception as e:
-                print('critical error - ', e)
-                exit()
-        else:
-            extensions = Path(file_in_question).suffixes
-            extensions = ''.join(extensions)
-            add_values(cursor, "DataBase", (path + SLASH + file, path, short_path, file,
-                                            extensions if extensions else str(None),
-                                            put_time_creation_to_table(file_in_question), os.path.getmtime(file_in_question)))
-
-    return
 
 
 def put_time_creation_to_table(path: str) -> float:
@@ -57,11 +18,6 @@ def put_time_creation_to_table(path: str) -> float:
 
 def get_time_creation_from_table(timestamp_number):
     return datetime.fromtimestamp(timestamp_number)
-
-
-# def print_result(cursor, request):
-#     cursor.execute(f"{request}")
-#     print(cursor.fetchall())
 
 
 def add_values(cursor, table, values: tuple):
@@ -101,37 +57,29 @@ def create_table(cursor):
 
 
 def is_admin():
-    try:
-        return ctypes.windll.shell32.IsUserAnAdmin()
-    except:
-        return False
+    return True if ctypes.windll.shell32.IsUserAnAdmin() else False
 
 
-def collect_data(root_directory=LOCAL_C):
-    operation = input("1: Create first data base; 2: Create second data base - ")
-    if operation == "1":
-        connection = sqlite3.connect(r'C:\Users\Leonid\PycharmProjects\filelists\firstChecking.db')
-    elif operation == "2":
-        connection = sqlite3.connect(r'C:\Users\Leonid\PycharmProjects\filelists\secondChecking.db')
-    else:
-        print("unexpected answer")
-        return
 
-    cursor = connection.cursor()
-    # drop_data_base(cursor)
-    create_table(cursor)
-
-    scan_directory(cursor, root_directory)
-    connection.commit()
-    connection.close()
-    print("Process complete[+]")
 
 
 def find_difference():
-    connection_to_firstChecking = sqlite3.connect(r'C:\Users\Leonid\PycharmProjects\filelists\firstChecking.db')
-    connection_to_secondChecking = sqlite3.connect(r'C:\Users\Leonid\PycharmProjects\filelists\secondChecking.db')
+    connection_to_firstChecking = sqlite3.connect(os.getcwd() + r'\firstChecking.db')
+    connection_to_secondChecking = sqlite3.connect(os.getcwd() + r'\secondChecking.db')
     cursor_firstChecking = connection_to_firstChecking.cursor()
     cursor_secondChecking = connection_to_secondChecking.cursor()
+
+    connection_to_db_difference = sqlite3.connect(os.getcwd() + r'\databaseDifference.db')
+    cursor_difference = connection_to_db_difference.cursor()
+
+    # cursor_difference.execute("""
+    # CREATE TABLE DataBase(
+    #         Number INTEGER ,
+    #         AddedFiles TEXT,
+    #         RemovedFiles TEXT
+    #     )
+    #
+    #                     """)
 
     cursor_firstChecking.execute('''
     SELECT Full_path FROM DataBase
@@ -154,8 +102,131 @@ def find_difference():
     print("Removed files ---", len(removed_files))
     print([name for name in removed_files])
 
+    for item in [name for name in added_files]:
+        cursor_difference.execute("""
+                INSERT INTO DataBase (
+                
+                            AddedFiles
+                        ) VALUES (?)
+                    """, (item,))
+    for item in [name for name in removed_files]:
+        cursor_difference.execute("""
+                INSERT INTO DataBase (
+                            RemovedFiles
+                        ) VALUES (?)
+                    """, (item,))
+    connection_to_db_difference.commit()
+    connection_to_db_difference.close()
     connection_to_firstChecking.close()
     connection_to_secondChecking.close()
+
+
+def convert_to_short_path(path: str) -> str:
+    buf = ctypes.create_unicode_buffer(260)
+    ctypes.windll.kernel32.GetShortPathNameW(r'\\?' + SLASH + path, buf, 260)
+    return buf.value
+
+
+def scan_directory(cursor, path):
+    short_path = convert_to_short_path(path)
+    try:
+        files = os.listdir(short_path)
+    except PermissionError:
+        print('Access denied[+] ', path, short_path)
+        return
+    except Exception as e:
+        print("unexpected error - ", e)
+        print(path, short_path)
+        exit()
+
+
+    for file in files:
+        file_in_question = short_path + SLASH + file
+
+        if os.path.isdir(file_in_question):
+            try:
+                add_values(cursor, "DataBase", (path + SLASH + file, path, short_path, file,
+                                                "directory", put_time_creation_to_table(file_in_question), os.path.getmtime(file_in_question)))
+                scan_directory(cursor, path + SLASH + file)
+            except Exception as e:
+                print('critical error - ', e)
+                exit()
+        else:
+            extensions = Path(file_in_question).suffixes
+            extensions = ''.join(extensions)
+            add_values(cursor, "DataBase", (path + SLASH + file, path, short_path, file,
+                                            extensions if extensions else "None",
+                                            put_time_creation_to_table(file_in_question), os.path.getmtime(file_in_question)))
+
+    return
+
+
+def collect_data(root_directory):
+    operation = input("1: Create first data base; 2: Create second data base - ")
+    if operation == "1":
+        connection = sqlite3.connect(r'C:\Users\Leonid\PycharmProjects\filelists\firstChecking.db')
+    elif operation == "2":
+        connection = sqlite3.connect(r'C:\Users\Leonid\PycharmProjects\filelists\secondChecking.db')
+    else:
+        print("unexpected answer")
+        return
+
+    cursor = connection.cursor()
+    # drop_data_base(cursor)
+    create_table(cursor)
+
+    scan_directory(cursor, root_directory)
+    connection.commit()
+    connection.close()
+    print("Process complete[+]")
+
+
+def collect_data_with_multiprocessing(root_directory: str):
+    operation = input("1: Create first data base; 2: Create second data base - ")
+    if operation == "1":
+        connection = sqlite3.connect(os.getcwd() + r'\firstChecking.db')
+    elif operation == "2":
+        connection = sqlite3.connect(os.getcwd() + r'\secondChecking.db')
+    else:
+        print("unexpected answer")
+        return
+
+    cursor = connection.cursor()
+    drop_data_base(cursor)
+    create_table(cursor)
+
+    for file in os.listdir(root_directory):
+
+        if os.path.isdir(root_directory + file):
+            line.put(file)
+        else:
+            print(file)
+            _short_path = convert_to_short_path(root_directory + SLASH + file)
+            extensions = Path(_short_path).suffixes
+            extensions = ''.join(extensions)
+            try:
+                _time_creation = put_time_creation_to_table(_short_path)
+                _time_modification = os.path.getmtime(_short_path)
+            except FileNotFoundError:
+                _time_creation = put_time_creation_to_table(root_directory + SLASH + file)
+                _time_modification = os.path.getmtime(root_directory + SLASH + file)
+
+            add_values(cursor, "DataBase", (root_directory + SLASH + file, root_directory, _short_path,
+                                            file, extensions if extensions else "None",
+                                            _time_creation, _time_modification))
+
+
+    while True:
+        try:
+            print(line.qsize())
+            print(line.get_nowait())
+        except queue.Empty:
+            break
+
+    # scan_directory(cursor, root_directory)
+    connection.commit()
+    connection.close()
+    print("Process complete[+]")
 
 
 def main():
@@ -165,10 +236,10 @@ def main():
             root_directory = input(r"Please, enter the root folder(default is C:\) - ")
             if root_directory == "":
                 root_directory = LOCAL_C
-                collect_data(root_directory)
+                collect_data_with_multiprocessing(root_directory)
                 break
             elif os.path.exists(root_directory):
-                collect_data(root_directory)
+                collect_data_with_multiprocessing(root_directory)
                 break
             else:
                 print("Entered path does not exist[+]")
@@ -185,10 +256,10 @@ def main():
 
 
 if __name__ == '__main__':
+    line = Queue()
+    process = Process()
     main()
 
     # if not is_admin():
     #     ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, " ".join(sys.argv), None, 1)
     #     sys.exit()
-
-
