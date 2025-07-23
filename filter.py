e18824a35d939cfc71e0dc8d7e60d94045ba2f44
ianadivane2006@gmail.com <ianadivane2006@gmail.com>
2025-07-23 22:24:18 +0300

    adedd new data base for filter.db - adedfiles, need refactor with method for adding time_creation.

--- filter.py
+++ filter.py
@@ -1,44 +1,45 @@
 from tqdm import tqdm
 from main import convert_time_stamp_to_time, SLASH
 import sqlite3
+import time
 
 
-def connect_to_filter_data_base():
-    def create_data_base():
-        cursor.execute("""
-                    CREATE TABLE ChangedFiles (
-                            Number INTEGER PRIMARY KEY AUTOINCREMENT,
-                            Full_path TEXT,
-                            Time_Before TEXT,
-                            Time_After TEXT,
-                            Time_stamp_before INTEGER,
-                            Time_stamp_after INTEGER
-                    )
-                        """)
-        return
-
-    def drop_data_base():
-        cursor.execute("""DROP TABLE ChangedFiles""")
-        return
+def drop_table(connection, cursor, table: str):
+    cursor.execute(f"""DROP TABLE {table}""")
+    connection.commit()
 
 
+def create_table(connection, cursor, table, **kwargs):
+    cursor.execute(f"""
+                CREATE TABLE {table} (
+                        Number INTEGER PRIMARY KEY AUTOINCREMENT
+                )
+                    """)
+    # print(kwargs)
+    for name_column in kwargs:
+        cursor.execute(f"""ALTER TABLE {table} ADD COLUMN {name_column} {kwargs[name_column]}""")
+
+    connection.commit()
+    return
+
+
+def filter_last_modification():
+    # initial connection and cursor
     connect_filter_db = sqlite3.connect(r'filter.db')
-    cursor = connect_filter_db.cursor()
-    drop_data_base()
-    create_data_base()
-    connect_filter_db.commit()
-
-    return connect_filter_db, cursor
+    cursor_filter_db = connect_filter_db.cursor()
 
 
-def filter_result():
-    connection_first_db = sqlite3.connect(r'firstChecking.db')
-    connection_second_db = sqlite3.connect(r'secondChecking.db')
+    # refactor database before work
+    drop_table(connection=connect_filter_db, cursor=cursor_filter_db, table="ChangedFiles")
+    create_table(connection=connect_filter_db, cursor=cursor_filter_db, table="ChangedFiles", **{
+                "Full_path": "TEXT",
+                "Time_Before": "TEXT",
+                "Time_After": "TEXT",
+                "Time_stamp_before": "INTEGER",
+                "Time_stamp_after": "INTEGER"
+            }
+                 )
 
-    cursor_first_db = connection_first_db.cursor()
-    cursor_second_db = connection_second_db.cursor()
-
-    connect_filter_db, cursor_filter_db = connect_to_filter_data_base()
 
     first_data = cursor_first_db.execute("SELECT Full_path, Date_creation_time_stamp FROM Database").fetchall()
     second_data = dict(cursor_second_db.execute("SELECT Full_path, Date_creation_time_stamp FROM Database").fetchall())
@@ -63,11 +64,68 @@ def filter_result():
     connect_filter_db.close()
     connection_first_db.commit()
     connection_second_db.commit()
-    connection_first_db.close()
-    connection_second_db.close()
+
+
+def filter_added_files():
+
+    connect_filter_db = sqlite3.connect(r'filter.db')
+    cursor_filter_db = connect_filter_db.cursor()
+    # Refactor table
+    drop_table(connection=connect_filter_db, cursor=cursor_filter_db, table="AddedFiles")
+    create_table(connect_filter_db, cursor_filter_db, "AddedFiles", **{
+        "Added_files": "TEXT",
+        "Time_creation": "TEXT"
+        }
+    )
+
+    added_files = (set(cursor_first_db.execute("""SELECT Full_path FROM DataBase""").fetchall()) -
+                   set(cursor_second_db.execute("""SELECT Full_path FROM DataBase""").fetchall()))
+
+    for added_file in tqdm(added_files):
+
+        start_time = time.time()
+        cursor_filter_db.execute("""INSERT INTO AddedFiles(
+            Added_files,
+            Time_creation
+        ) values(?, ?)""", (added_file[0], *cursor_first_db.execute(f"""SELECT Date_creation FROM DataBase WHERE Full_path = ?""", (added_file[0],)).fetchall()[0])
+                             )
+        # заметки по проге, сделать так чтобы премя создания файла шло вместе с его именем в словарь, бд слишком большая и потому
+        # каждый новый запрос сканит всю бд на поиск нужного значения, поэтому надо засунуть все в дикт и рабоать по ключам, а то что я сделал - гавно долгое
+
+        print(time.time() - start_time)
+
+    connect_filter_db.commit()
+    connect_filter_db.close()
+    return
+
 
 
 if __name__ == "__main__":
     # connect_to_filter_data_base()
-    filter_result()
+    connection_first_db = sqlite3.connect(r'firstChecking.db')
+    connection_second_db = sqlite3.connect(r'secondChecking.db')
+    cursor_first_db = connection_first_db.cursor()
+    cursor_second_db = connection_second_db.cursor()
 
+    # filter_last_modification()
+    filter_added_files()
+    connection_first_db.close()
+    connection_second_db.close()
+
+
+
+
+
+#
+# def create_data_base(connection, cursor, table, **kwargs):
+#     cursor.execute("""
+#                 CREATE TABLE ChangedFiles (
+#                         Number INTEGER PRIMARY KEY AUTOINCREMENT,
+#                         Full_path TEXT,
+#                         Time_Before TEXT,
+#                         Time_After TEXT,
+#                         Time_stamp_before INTEGER,
+#                         Time_stamp_after INTEGER
+#                 )
+#                     """)
+#     return
